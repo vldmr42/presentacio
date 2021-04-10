18e9c8473ba64a0f9da90e29bda65099c7670f88
manemejeff <kmb14056@yandex.ru>
2021-04-10 21:24:57 +0400

    added materialize framework files to static folder sidebar works

--- presentation/slides/static/slides/js/script.js
+++ presentation/slides/static/slides/js/script.js
@@ -1,4 +1,10 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
+    var options = {
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
     var instances = M.Sidenav.init(elems, options);
   });
